4c27964de95da033870f177403576f1388a21826
phnx2334 <carlos_i_13@hotmail.com>
2018-12-13 10:36:41 -0300

    alles klar

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person'
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1> Hi there </h1>
+                <Person />
+                <Person />
+                <Person />
+                <Person />
+                <Person />
+                <Person />
+            </div>
+        );
+    }
+}
+
+export default App;
