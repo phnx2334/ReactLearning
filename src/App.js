08cd9b17c08c82c602eb3681b49f520dad16c574
phnx2334 <carlos_i_13@hotmail.com>
2018-12-14 16:59:59 -0300

    a whole day of work

--- src/App.js
+++ src/App.js
@@ -3,14 +3,56 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+
+    state = {
+        persons: [
+            { name: "pachufla", age: 29 },
+            { name: "chicken", age: 39 },
+            { name: "lapika", age: 429 }
+        ]
+    }
+
+    onClickEventHandler = (newName) => {
+        console.log('received!')
+
+        this.setState({
+            persons: [
+                { name: newName, age: 48 },
+                { name: "chicken", age: 39 },
+                { name: "lapika", age: 5 }
+            ]
+        }
+        )
+    }
+
+    onInputChangeHandler = (event) => {
+        this.setState({
+            persons: [
+                { name: "paprika", age: 48 },
+                { name: event.target.value, age: 39 },
+                { name: "lapika", age: 5 }
+            ]
+        }
+        )
+    }
+
+
     render() {
         return (
             <div className="App">
                 <h1> Hi there </h1>
-                <Person name="Max" age="29"></Person>
-                <Person name="chicken" age="56">topekasldjhasdjhads</Person>
-                <Person name="platoon" age="352"></Person>
-
+                <button onClick={this.onClickEventHandler.bind(this, 'Plexator')}>Click me!</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}></Person>
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.onClickEventHandler.bind(this, 'Tryphomek')}
+                    changed={this.onInputChangeHandler}>topekasldjhasdjhads</Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}></Person>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Person from './Person/Person'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
